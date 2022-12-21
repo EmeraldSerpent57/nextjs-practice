bce87d4109d4f89d4b735dd91e13a2a141ecad30
Alaina Harrison <amvoce1221@gmail.com>
2022-12-21 12:35:26 -0800

    used Layout component as a wrapper for app pages

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,9 @@ const DUMMY_MEETUPS = [
   },
 ];
 function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+  return (
+      <MeetupList meetups={DUMMY_MEETUPS} />
+  );
 }
 
 export default HomePage;
