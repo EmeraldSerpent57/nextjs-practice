f54e58df1cc34c40fa9caf722c6861ba4014858e
Alaina Harrison <amvoce1221@gmail.com>
2022-12-22 12:34:37 -0800

    made sure meetupId could also get correct data from server

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,44 +1,38 @@
 // our-domain.com/new-meetup/[details]
-
+import { MongoClient, ObjectId } from "mongodb";
 import MeetupDetail from "../../components/meetups/MeetupDetails";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   
   return (
     <MeetupDetail
-      image="https://a.cdn-hotels.com/gdcs/production86/d278/b66ef277-f5ef-43c0-8aa3-aa0c9ed5b118.jpg?impolicy=fcrop&w=1600&h=1066&q=medium"
-      title="Italy Meetup"
-      address="Piazza di Trevi, 00187 Roma RM, Italy"
-      description="Meeting on the right side of the Trevi Fountain."
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 }
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://A57c1992:A57c1992@cluster0.esg3msz.mongodb.net/meetups?retryWrites=true&w=majority"
+  ); //never run this on the client side!!!
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find({}, {_id: 1}).toArray();     //getting all the meetup data
+
+  client.close();
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
-      },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-      {
-        params: {
-          meetupId: "m3",
-        },
-      },
-      {
-        params: {
-          meetupId: "m4",
-        },
-      },
-    ],
+    paths: meetups.map(meetup => ({
+      params: {
+        meetupId: meetup._id.toString(),
+      }
+    })), 
   };
 };
 
@@ -47,15 +41,27 @@ export async function getStaticProps(context) {
 
   const meetupId = context.params.meetupId;
 
+  const client = await MongoClient.connect(
+    "mongodb+srv://A57c1992:A57c1992@cluster0.esg3msz.mongodb.net/meetups?retryWrites=true&w=majority"
+  ); //never run this on the client side!!!
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  }); //needing access to a single meetup
+
+  client.close();
+
   return {
     props: {
       meetupData: {
-        image:
-          "https://a.cdn-hotels.com/gdcs/production86/d278/b66ef277-f5ef-43c0-8aa3-aa0c9ed5b118.jpg?impolicy=fcrop&w=1600&h=1066&q=medium",
-        id: meetupId,
-        title: "Italy Meetup",
-        address: "Piazza di Trevi, 00187 Roma RM, Italy",
-        description: "Meeting on the right side of the Trevi Fountain.",
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
       },
     },
   };
--- pages/index.js
+++ pages/index.js
@@ -3,43 +3,6 @@
 import { MongoClient } from "mongodb";
 import MeetupList from "../components/meetups/MeetupList";
 
-/*
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "Italy Meetup",
-    image:
-      "https://a.cdn-hotels.com/gdcs/production86/d278/b66ef277-f5ef-43c0-8aa3-aa0c9ed5b118.jpg?impolicy=fcrop&w=1600&h=1066&q=medium",
-    address: "Piazza di Trevi, 00187 Roma RM, Italy",
-    description: "Meeting on the right side of the Trevi Fountain.",
-  },
-  {
-    id: "m2",
-    title: "Germany Meetup",
-    image:
-      "https://neuschwansteintickets.com/images/disney-castle-neuschwanstein.jpg",
-    address: "Neuschwansteinstrabe 20, 87645 Schwangau, Germany",
-    description: "Meeting in the cafe of the Neuschwanstein Castle",
-  },
-  {
-    id: "m3",
-    title: "California, US Meetup",
-    image:
-      "https://national-park.com/wp-content/uploads/2016/04/Welcome-to-Channel-Islands-National-Park.jpg", //url, not file upload
-    address: "Ventura, CA 93001",
-    description: "Meeting in Channel Islands National Park",
-  },
-  {
-    id: "m4",
-    title: "Spain Meetup",
-    image:
-      "https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/ef/81/15.jpg",
-    address: "Calle Punta de Papagayo, 13, 35580 Yaiza, Las Palmas, Spain",
-    description: "Meeting in the Playa Blanca area, in the Canary Islands",
-  },
-];
-*/
-
 function HomePage(props) {
   return (
       <MeetupList meetups={props.meetups} />
@@ -78,7 +41,42 @@ export async function getStaticProps() {
 export default HomePage;
 
 
-
+/*
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "Italy Meetup",
+    image:
+      "https://a.cdn-hotels.com/gdcs/production86/d278/b66ef277-f5ef-43c0-8aa3-aa0c9ed5b118.jpg?impolicy=fcrop&w=1600&h=1066&q=medium",
+    address: "Piazza di Trevi, 00187 Roma RM, Italy",
+    description: "Meeting on the right side of the Trevi Fountain.",
+  },
+  {
+    id: "m2",
+    title: "Germany Meetup",
+    image:
+      "https://neuschwansteintickets.com/images/disney-castle-neuschwanstein.jpg",
+    address: "Neuschwansteinstrabe 20, 87645 Schwangau, Germany",
+    description: "Meeting in the cafe of the Neuschwanstein Castle",
+  },
+  {
+    id: "m3",
+    title: "California, US Meetup",
+    image:
+      "https://national-park.com/wp-content/uploads/2016/04/Welcome-to-Channel-Islands-National-Park.jpg", //url, not file upload
+    address: "Ventura, CA 93001",
+    description: "Meeting in Channel Islands National Park",
+  },
+  {
+    id: "m4",
+    title: "Spain Meetup",
+    image:
+      "https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/ef/81/15.jpg",
+    address: "Calle Punta de Papagayo, 13, 35580 Yaiza, Las Palmas, Spain",
+    description: "Meeting in the Playa Blanca area, in the Canary Islands",
+  },
+];
+*/
 
 
 /* Server-side Rendering example ONLY, not used here
