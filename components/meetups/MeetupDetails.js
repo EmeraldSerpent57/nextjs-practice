ae58932af68b5afca0301661b4ba833aa3ad9f0a
Alaina Harrison <amvoce1221@gmail.com>
2023-01-18 22:30:35 -0800

    added back button

--- components/meetups/MeetupDetails.js
+++ components/meetups/MeetupDetails.js
@@ -1,17 +1,25 @@
+import { Router, useRouter } from 'next/router';
 import classes from './MeetupDetail.module.css';
 
 function MeetupDetail(props) {
+  const router = useRouter();
+  const goBackHandler = () => {
+    router.push("/");
+  };
+
   return (
-    <section className={classes.detail}>
-      <img
-        src={props.image}
-        alt={props.title}
-      />
-      <h1>{props.title}</h1>
-      <h4>When: {props.date}</h4>
-      <address>Where: {props.address}</address>
-      <p>{props.description}</p>
-    </section>
+    <>
+      <section className={classes.detail}>
+        <img src={props.image} alt={props.title} />
+        <h1>{props.title}</h1>
+        <h4>When: {props.date}</h4>
+        <address>Where: {props.address}</address>
+        <p>{props.description}</p>
+      </section>
+      <footer className={classes.btn}>
+        <button onClick={goBackHandler}>Back</button>
+      </footer>
+    </>
   );
 }
 
